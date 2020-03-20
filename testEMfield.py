cb2f84b3737761c9c7d2803925c4b24c8460af74
Jack-Fannon <61186010+Jack-Fannon@users.noreply.github.com>
2020-03-20 15:41:12 +0000

    Update testEMfield.py

--- testEMfield.py
+++ testEMfield.py
@@ -3,15 +3,11 @@ import math
 import copy
 import scipy.constants
 import pytest
+import unittest
 from EMField import EMField
 from Chargedparticle import Proton
 
-class TestEMField:
-
-    def __init__(self):
-        self.field=0
-        self.proton=0
-        self.time=0
+class TestEMField(unittest.TestCase):
 
     def setUp(self):
         self.field = EMField(np.array([0,0,0.0000001], dtype=float), np.array([0.0,0.00001,0.0], dtype=float))
@@ -19,4 +15,6 @@ class TestEMField:
         self.time = 5        
 
     def test_lorentzaccel(self):
-        assert(self.field.lorentzaccel(self.proton, self.time) == np.array([9578.83322415,-667.00632589,0.0])).all()
+        result = self.field.lorentzaccel(self.proton, self.time)
+        expected = np.array([9578.83322415,-667.00632589,0.0])
+        assert (np.isclose(result, expected)).all()
